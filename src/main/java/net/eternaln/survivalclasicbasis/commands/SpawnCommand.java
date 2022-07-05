062862970cc136dd367fa63cbeb9c777d3eedadd
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-07-05 00:38:13 -0300

    feat: modified cooldown manager

--- src/main/java/net/eternaln/survivalclasicbasis/commands/SpawnCommand.java
+++ src/main/java/net/eternaln/survivalclasicbasis/commands/SpawnCommand.java
@@ -1,78 +1,27 @@
 package net.eternaln.survivalclasicbasis.commands;
 
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.Default;
 import net.eternaln.survivalclasicbasis.SurvivalClasicBasis;
-import net.eternaln.survivalclasicbasis.utils.CooldownManager;
-import net.eternaln.survivalclasicbasis.utils.LocationUtil;
+import net.eternaln.survivalclasicbasis.utils.Cooldown;
 import net.eternaln.survivalclasicbasis.utils.Utils;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
-public class SpawnCommand implements CommandExecutor {
+import java.util.UUID;
 
-    private final SurvivalClasicBasis plugin;
+public class SpawnCommand extends BaseCommand {
 
-    private final CooldownManager cooldownManager = new CooldownManager();
+    private final Cooldown<UUID> cooldown = new Cooldown<>(SurvivalClasicBasis.getConfiguration().getCmdCooldown());
 
-    public SpawnCommand(SurvivalClasicBasis instance) {
-        plugin = instance;
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        FileConfiguration config = plugin.getConfig();
-
-        if (!(sender instanceof Player)) {
-            if (args.length == 0) {
-                Utils.log("&cDebes ser un usuario para usar esto");
-                return false;
-            }
-            return false;
+    @Default
+    public void spawn(Player sender) {
+        if (!sender.hasPermission("survivalclasicbasis.cooldown.bypass") && !cooldown.isCooledDown(sender.getUniqueId())) {
+            sender.sendMessage(Utils.ct("&cDebes esperar &b" + cooldown.getSecondsRemaining(sender.getUniqueId()) + " &csegundos"));
+            return;
         }
 
-        Player player = (Player) sender;
-
-        if (args.length == 0) {
-            int timeLeft = cooldownManager.getCooldown(player.getUniqueId());
-
-            if(config.getString("spawn-location") == null) {
-                Utils.send(player, "&cAvisa a un staff");
-                return true;
-            }
-
-            if (!player.hasPermission("survivalclasicbasis.cooldown.bypass")) {
-                if(LocationUtil.parseToLocation(config.getString("spawn-location")) != null) {
-                    if (timeLeft == 0) {
-                        player.teleport(LocationUtil.parseToLocation(config.getString("spawn-location")));
-                        Utils.send(player, config.getString("spawn"));
-                        cooldownManager.setCooldown(player.getUniqueId(), CooldownManager.DEFAULT_COOLDOWN);
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                int timeLeft = cooldownManager.getCooldown(player.getUniqueId());
-                                cooldownManager.setCooldown(player.getUniqueId(), --timeLeft);
-                                if (timeLeft == 0) {
-                                    this.cancel();
-                                }
-                            }
-                        }.runTaskTimer(this.plugin, 20, 20);
-
-                        return false;
-                    } else {
-                        player.sendMessage(Utils.ct("&cDebes esperar &b" + timeLeft + " &csegundos"));
-                    }
-                }
-                return true;
-            } else {
-                player.teleport(LocationUtil.parseToLocation(config.getString("spawn-location")));
-                Utils.send(player, config.getString("spawn"));
-            }
-
-            return false;
-        }
-        return false;
+        sender.teleport(SurvivalClasicBasis.getConfiguration().getSpawnLocation());
+        cooldown.addToCooldown(sender.getUniqueId());
+        Utils.send(sender, SurvivalClasicBasis.getConfiguration().getTpSpawn());
     }
 }
---
+++ src/main/java/net/eternaln/survivalclasicbasis/utils/Cooldown.java
@@ -0,0 +1,66 @@
+package net.eternaln.survivalclasicbasis.utils;
+
+import java.util.HashMap;
+
+/**
+ * An easy to use Cooldown class
+ */
+public class Cooldown<T> {
+
+    private final HashMap<T, Long> cooldowns = new HashMap<>();
+    // Change time to how ever long you want
+    private final long time;
+
+    /**
+     * Instantiates a new Cooldown.
+     *
+     * @param time the time in milliseconds
+     */
+    public Cooldown(long time) {
+        this.time = time;
+    }
+
+    /**
+     * Add the player to the cooldown
+     *
+     * @param player the player
+     */
+    public void addToCooldown(T player) {
+        cooldowns.put(player, System.currentTimeMillis());
+    }
+
+    /**
+     * Get the remaining time
+     *
+     * @param player the player
+     * @return time remaining
+     */
+    public long getTimeRemaining(T player) {
+        return time - ((System.currentTimeMillis() - cooldowns.get(player)));
+    }
+
+    /**
+     * Get the remaining seconds
+     *
+     * @param player the player
+     * @return time remaining
+     */
+    public long getSecondsRemaining(T player) {
+        return time - ((System.currentTimeMillis() - cooldowns.get(player)) / 1000);
+    }
+
+    /**
+     * Gets if the player's cooldown is over
+     *
+     * @param player the player
+     * @return boolean
+     */
+    public boolean isCooledDown(T player) {
+        if (!cooldowns.containsKey(player) || (((System.currentTimeMillis() - cooldowns.get(player))) >= time)) {
+            cooldowns.remove(player);
+            return true;
+        } else
+            return false;
+    }
+
+}
