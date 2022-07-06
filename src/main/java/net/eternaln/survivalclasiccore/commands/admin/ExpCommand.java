ba0603ba2afc8079dfb09cbb66571d02fd088785
iTito420 <aitorarias19@gmail.com>
2022-07-06 20:46:00 +0200

    fix: exp command finished

--- src/main/java/net/eternaln/survivalclasiccore/commands/admin/ExpCommand.java
+++ src/main/java/net/eternaln/survivalclasiccore/commands/admin/ExpCommand.java
@@ -12,38 +12,40 @@ public class ExpCommand extends BaseCommand {
 
     @Subcommand("add|añadir")
     public class GiveExpSubCommand extends BaseCommand {
-        @Subcommand("me|yo")
-        public void give(Player sender, int amount) {
-            sender.giveExp(amount);
-            sender.playSound(sender.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+        @Subcommand("amount|cantidad")
+        public class AmountExpSubCommand extends BaseCommand {
+            @Subcommand("me|yo")
+            public void give(Player sender, int amount) {
+                sender.giveExp(amount);
+                sender.playSound(sender.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+            }
+
+            @Subcommand("other|otro|otros|others")
+            @CommandPermission("survivalclasiccore.exp.other")
+            @CommandCompletion("@players")
+            public void giveOther(String target, int amount) {
+                Player targetPlayer = Bukkit.getPlayer(target);
+                targetPlayer.giveExp(amount);
+                targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+            }
         }
 
-        @Subcommand("other|otro|otros|others")
-        @CommandPermission("survivalclasiccore.exp.other")
-        @CommandCompletion("@players")
-        public void giveOther(String target, int amount) {
-            Player targetPlayer = Bukkit.getPlayer(target);
-            targetPlayer.giveExp(amount);
-            targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
-        }
-    }
+        @Subcommand("level|nivel")
+        public class LevelExpSubCommand extends BaseCommand {
+            @Subcommand("me|yo")
+            public void give(Player sender, int amount) {
+                sender.giveExpLevels(amount);
+                sender.playSound(sender.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+            }
 
-    @Subcommand("remove|quitar|restar|restarxp|restarexp")
-    public class RemoveExpSubCommand extends BaseCommand {
-        @Subcommand("me|yo")
-        public void remove(Player sender, int amount) {
-            int currentExp = sender.getTotalExperience();
-            sender.setExp(currentExp - amount);
-            sender.playSound(sender.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
-        }
-
-        @Subcommand("other|otro|otros|others")
-        @CommandPermission("survivalclasiccore.exp.other")
-        @CommandCompletion("@players")
-        public void removeOther(String target, int amount) {
-            Player targetPlayer = Bukkit.getPlayer(target);
-            targetPlayer.setExp(targetPlayer.getTotalExperience() - amount);
-            targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+            @Subcommand("other|otro|otros|others")
+            @CommandPermission("survivalclasiccore.exp.other")
+            @CommandCompletion("@players")
+            public void giveOther(String target, int amount) {
+                Player targetPlayer = Bukkit.getPlayer(target);
+                targetPlayer.giveExpLevels(amount);
+                targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+            }
         }
     }
 
@@ -51,7 +53,8 @@ public class ExpCommand extends BaseCommand {
     public class SetExpSubCommand extends BaseCommand {
         @Subcommand("me|yo")
         public void set(Player sender, int amount) {
-            sender.setExp(amount);
+            sender.setExp(0);
+            sender.setLevel(amount);
             sender.playSound(sender.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
         }
 
@@ -60,7 +63,29 @@ public class ExpCommand extends BaseCommand {
         @CommandCompletion("@players")
         public void setOther(String target, int amount) {
             Player targetPlayer = Bukkit.getPlayer(target);
-            targetPlayer.setExp(amount);
+            targetPlayer.setExp(0);
+            targetPlayer.setLevel(amount);
+            targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+        }
+    }
+
+    @Subcommand("clear|limpiar")
+    public class RemoveExpSubCommand extends BaseCommand {
+        @Subcommand("me|yo")
+        public void remove(Player sender) {
+            int currentExp = sender.getTotalExperience();
+            sender.setExp(0);
+            sender.setLevel(0);
+            sender.playSound(sender.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+        }
+
+        @Subcommand("other|otro|otros|others")
+        @CommandPermission("survivalclasiccore.exp.other")
+        @CommandCompletion("@players")
+        public void removeOther(String target) {
+            Player targetPlayer = Bukkit.getPlayer(target);
+            targetPlayer.setExp(0);
+            targetPlayer.setLevel(0);
             targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
         }
     }
