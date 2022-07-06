1b7b7e6e68d3a21f0e8538ced4f13de8e8ff216a
Aitooor <aitorarias19@gmail.com>
2022-07-06 22:42:58 +0200

    fix: GamemodeCommand

---
+++ src/main/java/net/eternaln/survivalclasiccore/commands/admin/gamemode/GamemodeCommand.java
@@ -0,0 +1,66 @@
+package net.eternaln.survivalclasiccore.commands.admin.gamemode;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.CommandHelp;
+import co.aikar.commands.annotation.*;
+import net.eternaln.survivalclasiccore.utils.Utils;
+import org.bukkit.GameMode;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandAlias("gamemode|gm|modo")
+@CommandPermission("survivalclasiccore.gm")
+public class GamemodeCommand extends BaseCommand {
+
+    @Default
+    @CatchUnknown
+    @HelpCommand("help|ayuda")
+    public void help(CommandSender sender, CommandHelp help) {
+        help.showHelp();
+    }
+
+    @Subcommand("survival|s|supervivencia|0")
+    @CommandPermission("survivalclasiccore.gm.survival")
+    public void setSurvival(Player sender) {
+        if(sender.getGameMode() != GameMode.SURVIVAL) {
+            sender.setGameMode(GameMode.SURVIVAL);
+            Utils.send(sender, "&fHas cambiado tu modo de juego a &eSurvival");
+        } else {
+            Utils.send(sender, "&fYa tienes el modo de juego &eSurvival");
+        }
+    }
+
+    @Subcommand("creative|c|creacion|1")
+    @CommandPermission("survivalclasiccore.gm.creative")
+    public void setCreative(Player sender) {
+        if(sender.getGameMode() != GameMode.CREATIVE) {
+            sender.setGameMode(GameMode.CREATIVE);
+            Utils.send(sender, "&fHas cambiado tu modo de juego a &eCreative");
+        } else {
+            Utils.send(sender, "&fYa tienes el modo de juego &eCreative");
+        }
+    }
+
+    @Subcommand("adventure|a|aventura|2")
+    @CommandPermission("survivalclasiccore.gm.adventure")
+    public void setAdventure(Player sender) {
+        if(sender.getGameMode() != GameMode.ADVENTURE) {
+            sender.setGameMode(GameMode.ADVENTURE);
+            Utils.send(sender, "&fHas cambiado tu modo de juego a &eAdventure");
+        } else {
+            Utils.send(sender, "&fYa tienes el modo de juego &eAdventure");
+        }
+    }
+
+    @Subcommand("spectator|s|spectator|3")
+    @CommandPermission("survivalclasiccore.gm.spectator")
+    public void setSpectator(Player sender) {
+        if(sender.getGameMode() != GameMode.SPECTATOR) {
+            sender.setGameMode(GameMode.SPECTATOR);
+            Utils.send(sender, "&fHas cambiado tu modo de juego a &eEspectador");
+        } else {
+            Utils.send(sender, "&fYa tienes el modo de juego &eEspectador");
+        }
+    }
+
+}
---
+++ src/main/java/net/eternaln/survivalclasiccore/commands/admin/gamemode/GamemodeOtherCommand.java
@@ -0,0 +1,106 @@
+package net.eternaln.survivalclasiccore.commands.admin.gamemode;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.CommandHelp;
+import co.aikar.commands.annotation.*;
+import net.eternaln.survivalclasiccore.utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+
+@CommandAlias("gamemodeto|gmto|modoa|gmother|gamemodeother")
+@CommandPermission("survivalclasiccore.gm.other")
+public class GamemodeOtherCommand extends BaseCommand {
+
+    @Default
+    @CatchUnknown
+    @HelpCommand("help|ayuda")
+    public void help(CommandHelp help) {
+        help.showHelp();
+    }
+
+    @Subcommand("survival|s|supervivencia|0")
+    @CommandCompletion("@players")
+    public void setSurvival(Player sender, String target) {
+        Player targetPlayer = Bukkit.getPlayer(target);
+        if(!(targetPlayer == null || !targetPlayer.isOnline())) {
+            if(targetPlayer.getGameMode() != GameMode.SURVIVAL) {
+                if(targetPlayer != sender) {
+                    targetPlayer.setGameMode(GameMode.SURVIVAL);
+                    Utils.send(sender, "&fHas cambiado el modo de juego de &b" + targetPlayer.getName() + " &fa &eSurvival");
+                    Utils.send(targetPlayer, "&fTu modo de juego ha sido cambiado por &b" + sender.getName() + " &fa &eSurvival");
+                } else {
+                    Utils.send(sender, "&fNo puedes cambiar tu propio modo de juego");
+                }
+            } else {
+                Utils.send(sender, "&fEl jugador &b" + targetPlayer.getName() + " &fya tiene el modo de juego &eSurvival");
+            }
+        } else {
+            Utils.send(sender, "&fEl jugador &b" + target + " &fno esta online");
+        }
+    }
+
+    @Subcommand("creative|c|creacion|1")
+    @CommandCompletion("@players")
+    public void setCreative(Player sender, String target) {
+        Player targetPlayer = Bukkit.getPlayer(target);
+        if(!(targetPlayer == null || !targetPlayer.isOnline())) {
+            if(targetPlayer.getGameMode() != GameMode.CREATIVE) {
+                if(targetPlayer != sender) {
+                    targetPlayer.setGameMode(GameMode.CREATIVE);
+                    Utils.send(sender, "&fHas cambiado el modo de juego de &b" + targetPlayer.getName() + " &fa &eCreative");
+                    Utils.send(targetPlayer, "&fTu modo de juego ha sido cambiado por &b" + sender.getName() + " &fa &eCreative");
+                } else {
+                    Utils.send(sender, "&fNo puedes cambiar tu propio modo de juego");
+                }
+            } else {
+                Utils.send(sender, "&fEl jugador &b" + targetPlayer.getName() + " &fya tiene el modo de juego &eCreative");
+            }
+        } else {
+            Utils.send(sender, "&fEl jugador &b" + target + " &fno esta online");
+        }
+    }
+
+    @Subcommand("adventure|a|aventura|2")
+    @CommandCompletion("@players")
+    public void setAdventure(Player sender, String target) {
+        Player targetPlayer = Bukkit.getPlayer(target);
+        if(!(targetPlayer == null || !targetPlayer.isOnline())) {
+            if(targetPlayer.getGameMode() != GameMode.ADVENTURE) {
+                if(targetPlayer != sender) {
+                    targetPlayer.setGameMode(GameMode.ADVENTURE);
+                    Utils.send(sender, "&fHas cambiado el modo de juego de &b" + targetPlayer.getName() + " &fa &eAdventure");
+                    Utils.send(targetPlayer, "&fTu modo de juego ha sido cambiado por &b" + sender.getName() + " &fa &eAdventure");
+                } else {
+                    Utils.send(sender, "&fNo puedes cambiar tu propio modo de juego");
+                }
+            } else {
+                Utils.send(sender, "&fEl jugador &b" + targetPlayer.getName() + " &fya tiene el modo de juego &eAdventure");
+            }
+        } else {
+            Utils.send(sender, "&fEl jugador &b" + target + " &fno esta online");
+        }
+    }
+
+    @Subcommand("spectator|s|spectator|3")
+    @CommandCompletion("@players")
+    public void setSpectator(Player sender, String target) {
+        Player targetPlayer = Bukkit.getPlayer(target);
+        if(!(targetPlayer == null || !targetPlayer.isOnline())) {
+            if(targetPlayer.getGameMode() != GameMode.SPECTATOR) {
+                if(targetPlayer != sender) {
+                    targetPlayer.setGameMode(GameMode.SPECTATOR);
+                    Utils.send(sender, "&fHas cambiado el modo de juego de &b" + targetPlayer.getName() + " &fa &eEspectador");
+                    Utils.send(targetPlayer, "&fTu modo de juego ha sido cambiado por &b" + sender.getName() + " &fa &eEspectador");
+                } else {
+                    Utils.send(sender, "&fNo puedes cambiar tu propio modo de juego");
+                }
+            } else {
+                Utils.send(sender, "&fEl jugador &b" + targetPlayer.getName() + " &fya tiene el modo de juego &eEspectador");
+            }
+        } else {
+            Utils.send(sender, "&fEl jugador &b" + target + " &fno esta online");
+        }
+    }
+
+}
