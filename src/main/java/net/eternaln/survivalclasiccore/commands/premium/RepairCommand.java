34ad2b60f75e81143f99602b26202489d8c022d9
iTito420 <aitorarias19@gmail.com>
2022-07-27 01:11:19 +0200

    fix: repair command cooldown

--- src/main/java/net/eternaln/survivalclasiccore/commands/premium/RepairCommand.java
+++ src/main/java/net/eternaln/survivalclasiccore/commands/premium/RepairCommand.java
@@ -19,8 +19,8 @@ import java.util.concurrent.TimeUnit;
 @CommandPermission("survivalclasic.repair")
 public class RepairCommand extends BaseCommand {
 
-    private MessagesFile messageFile = SurvivalClasicCore.getMessagesFile();
-    private Cooldown<UUID> cooldown = new Cooldown<>(SurvivalClasicCore.getConfiguration().getRepairCooldown(), TimeUnit.SECONDS);
+    private final MessagesFile messageFile = SurvivalClasicCore.getMessagesFile();
+    private final Cooldown<UUID> cooldown = new Cooldown<>(SurvivalClasicCore.getConfiguration().getRepairCooldown(), TimeUnit.MINUTES);
 
     @Default
     public void repair(Player sender) {
@@ -33,16 +33,15 @@ public class RepairCommand extends BaseCommand {
         ItemMeta itemMeta = item.getItemMeta();
         if(material.isBlock()) {
             Utils.send(sender, "&cNo se puede reparar");
-            return;
         } else {
             if (itemMeta instanceof Damageable) {
-                if (material.getMaxDurability() >= 1) {
+                if (material.getMaxDurability() < 1) {
                     Utils.send(sender, "&cYa esta reparado");
-                    return;
                 } else {
                     if (!cooldown.isCooldownOverRepair(sender.getUniqueId())) {
-                        String cooldownTime = cooldown.getSecondsRemainingStringRepair(sender.getUniqueId());
-                        Utils.send(sender, messageFile.cooldown.replace("%time%", cooldownTime));
+                        String cooldowns = cooldown.getSecondsRemainingStringRepair(sender.getUniqueId());
+
+                        Utils.send(sender, messageFile.cooldown.replace("%time%", cooldowns));
                         return;
                     }
                     cooldown.addToCooldownRepair(sender.getUniqueId());
@@ -51,53 +50,12 @@ public class RepairCommand extends BaseCommand {
                     damageable.setDamage(0);
                     item.setItemMeta(damageable);
                     Utils.send(sender, "&aReparado");
-                    return;
                 }
             } else {
                 Utils.send(sender, "&cNo se puede reparar");
-                return;
             }
         }
     }
 
-    @Subcommand("otros|others|other|otro")
-    @CommandPermission("survivalclasic.repair.other")
-    public void other(Player sender, Player target) {
-        ItemStack item = target.getInventory().getItemInMainHand();
-        if (item == null || item.getType() == Material.AIR) {
-            Utils.send(sender, "&cNo tienes nada en la mano");
-            return;
-        }
-        Material material = target.getInventory().getItemInMainHand().getType();
-        ItemMeta itemMeta = item.getItemMeta();
-        if(material.isBlock()) {
-            Utils.send(sender, "&cNo se puede reparar");
-            return;
-        } else {
-            if (itemMeta instanceof Damageable) {
-                if (material.getMaxDurability() >= 1) {
-                    Utils.send(sender, "&cYa esta reparado");
-                    return;
-                } else {
-                    if (!cooldown.isCooldownOver(sender.getUniqueId())) {
-                        String cooldownTime = cooldown.getSecondsRemainingString(sender.getUniqueId());
-                        Utils.send(sender, messageFile.cooldown.replace("%time%", cooldownTime));
-                        return;
-                    }
-                    cooldown.addToCooldown(sender.getUniqueId());
-
-                    Damageable damageable = (Damageable) itemMeta;
-                    damageable.setDamage(0);
-                    item.setItemMeta(damageable);
-                    Utils.send(sender, "&fReparado &b" + target.getName());
-                    Utils.send(target, sender.getDisplayName() + " &fha reparado tu herramienta");
-                    return;
-                }
-            } else {
-                Utils.send(sender, "&cNo se puede reparar");
-                return;
-            }
-        }
-    }
 
 }
